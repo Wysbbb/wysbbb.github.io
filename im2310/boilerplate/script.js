fa99e77c6ccd271c4ce860fa6507a70dec1f36ab
Rhys <97144664+Wysbbb@users.noreply.github.com>
2023-06-02 12:30:48 +1000

    Add files via upload

--- im2310/boilerplate/script.js
+++ im2310/boilerplate/script.js
@@ -1,35 +1,37 @@
-
-const content = document.querySelector(".distort");
+const contents = document.querySelectorAll(".distort");
 let currentPos = window.pageYOffset;
+const maxScrollHeight = document.documentElement.scrollHeight - window.innerHeight;
 
 const callDistort = function() {
     const newPos = window.pageYOffset;
     const diff = newPos - currentPos;
     const speed = diff * 0.35;
 
-    content.style.transform = "skewY(" + speed + "deg)";
-    currentPos = newPos;
+    contents.forEach((content) => {
+        content.style.transform = "skewY(" + speed + "deg)";
+    });
 
-    console.log("test");
+    currentPos = newPos;
 };
 
-
-
-
 let text = document.getElementById('text');
-let treeLeft = document.getElementById('tree-left');
-let treeRight = document.getElementById('tree-right');
-let cloudLeft = document.getElementById('cloud-left');
-let cloudRight = document.getElementById('cloud-right');
-
+let lightLeft = document.getElementById('light-left');
+let lightRight = document.getElementById('light-right');
+let gateLeft = document.getElementById('gate-left');
+let gateRight = document.getElementById('gate-right');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-requestAnimationFrame(callDistort);
+
+    if (window.scrollY >= maxScrollHeight) {
+        return; 
+    }
+
+    requestAnimationFrame(callDistort);
 
     text.style.marginTop = value * 2.5 + 'px';
-    treeLeft.style.left = value * -1.5 + 'px';
-    treeRight.style.left = value * 1.5 + 'px';
-    cloudLeft.style.left = value * 0.5 + 'px';
-    cloudRight.style.left = value * -0.5 + 'px';
-}, {passive: true});
+    lightLeft.style.left = value * -1.5 + 'px';
+    lightRight.style.left = value * 1.5 + 'px';
+    gateLeft.style.left = value * 0.5 + 'px';
+    gateRight.style.left = value * -0.5 + 'px';
+}, { passive: true });
